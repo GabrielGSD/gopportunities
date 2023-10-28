3c2c57721590b54d371cdc7e52f4f686ea76742b
GabrielGSD <gabrieldaniel@gec.inatel.br>
2023-10-28 14:31:59 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/GabrielGSD/gopportunities/router"
+import (
+	"github.com/GabrielGSD/gopportunities/config"
+	"github.com/GabrielGSD/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize the configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing configs: %v", err)
+		// panic(err)
+		return
+	}
+
 	router.Initialize()
 }
